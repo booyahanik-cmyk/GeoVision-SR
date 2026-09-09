287622da4a57d9b6ec19781ebe90851ca21d7755
satyajitmishra-dev <satyajitmishra791@gmail.com>
2026-09-10 00:11:01 +0530

    initialize Spring Boot backend project structure with Maven wrapper and essential dependencies

---
+++ server/geovision-backend/src/main/java/team/nexaura/geovision_backend/GeovisionBackendApplication.java
@@ -0,0 +1,13 @@
+package team.nexaura.geovision_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeovisionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeovisionBackendApplication.class, args);
+	}
+
+}
